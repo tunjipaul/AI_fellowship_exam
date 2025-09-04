175b82b319b68228314d2536d33fadfcbfc0f680
TunjiPaul <ogorpaul877@gmail.com>
2025-09-04 15:45:07 +0100

    Paul_Ogor_Branch

--- Paul_Ogor_Exam.py
+++ Paul_Ogor_Exam.py
@@ -20,26 +20,29 @@ def denominator_error(b):
     
 
 
+    
+
+
 
 
 while True:
-    print("Choose Operation; (+, -, *, /) or type Exit to quit: ")
-    num_1 = float(input("Enter first number: "))
-    num_2 = float(input("Enter Second Number: "))
-    choice = input("Select an operation; (+, -, *, /) or type Exit to quit: ").strip().lower()
     
     
     try:
-        if choice == int:
-            pass
-        elif choice == "+":
-            print(f"The sum of {num_1} + {num_2} = {num_1 + num_2} ")
+        print("Choose Operation; (+, -, *, /) or type Exit to quit: ")
+        num_1 = float(input("Enter first number: "))
+        num_2 = float(input("Enter Second Number: "))
+    
+        choice = input("Select an operation; (+, -, *, /) or type Exit to quit: ").strip().lower()
+    
+        if choice == "+":
+            print(f"The sum of {num_1} + {num_2} = {add_task(num_1, num_2)} ")
         elif choice == "-":
-            print(f"The subtraction of {num_1} - {num_2} = {num_1 - num_2} ")
+            print(f"The subtraction of {num_1} - {num_2} = {subtract_task(num_1, num_2)} ")
         elif choice == "*":
-            print(f"The multiplication of {num_1} * {num_2} = {num_1 * num_2} ")
+            print(f"The multiplication of {num_1} * {num_2} = {multiply_task(num_1, num_2)} ")
         elif choice == "/":
-            print(f"The division of {num_1} / {num_2} = {num_1 / num_2} ")
+            print(f"The division of {num_1} / {num_2} = {divide(num_1, num_2)} ")
         elif choice == "exit":
             print(f"Exiting Calculator... Goodbye!")
             break
@@ -51,31 +54,32 @@ while True:
         print("error:", e)
     
 
-# #question 2
-# while True:
-#     user_input = input("Enter a number (or type 'exit' to quit): ")
-#     if user_input == "exit":
-#         print("Goodbye!")
-#         break       # break out of loop
+#question 2
+while True:
+    user_input = input("Enter a number (or type 'exit' to quit): ")
+    if user_input == "exit":
+        print("Goodbye!")
+        break       # break out of loop
     
-#     num = int(user_input)   # convert to integer
+    num = int(user_input)   # convert to integer
     
-#     if num % 2 == 0:
-#         print("The number is even")
-#     else:
-#         print("The number is odd")
+    if num % 2 == 0:
+        print("The number is even")
+    else:
+        print("The number is odd")
 
-# #question 3.
-# while True:
-#     age = float(input("Enter your age (or type exit to quit): "))
-#     try:
-#      if age >= 18:
-#         print("You can vote")
-#      elif age == "exit":
-#         print("Goodbye!")
-#         break
-#      else:
-#             print("You cannot vote")
+#question 3.
+while True:
+    age = float(input("Enter your age (or type exit to quit): "))
+    try:
+     if age >= 18:
+        print("You can vote")
+     elif age == "exit":
+        print("Goodbye!")
+        break
+     else:
+            print("You cannot vote")
     
-#     except:
-#      print("Invalid input")
+    except:
+     print("Invalid input")
+     
